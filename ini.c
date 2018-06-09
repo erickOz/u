f46c3536320ba546ebac1923cfdd7c64f838e0c4
erickOz <40083230+erickOz@users.noreply.github.com>
2018-06-09 00:34:02 -0500

    Create ini.c

---
+++ ini.c
@@ -0,0 +1,21 @@
+# u
+CodigoVariadoAlgo
+#Este codigo iniciará con la implementación de listas enlazadas
+#y luego se añadirá la implementación del método de ordenación
+#MergeSort y #QuickSort ambos muy eficientes y claramente diferenciados
+typedef int TInfo;
+typedef struct nodo{
+  TInfo elem;
+  struct nodo* next;
+}TNodo;
+typedef struct lista{
+  struct nodo* head;
+  struct nodo* tail;
+}TLista;
+
+//Metodos de las listas enlazadas
+void initialize(TLista* lista)
+{
+  lista->head=NULL;
+  lista->tail=NULL;
+}
